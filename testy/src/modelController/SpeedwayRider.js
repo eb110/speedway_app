1bdccd2d92c7b22273e398301160e74eb7bae0ca
eb110 <wfigura@op.pl>
2022-10-24 12:54:17 +0100

    #00 parser update

--- testy/src/modelController/SpeedwayRider.js
+++ testy/src/modelController/SpeedwayRider.js
@@ -1,39 +1,39 @@
-export default class SpeedwayRider{
+export default class SpeedwayRider {
 
-    controller(){}
+    controller() { }
 
     getTheRiderById = async (id) => {
-        try{
+        try {
             return await fetch(`http://localhost:8080/rider/getById/${id}`)
-            .then((res) => {
-                if(res.status === 200){
-                    return res.json()
-                }
-                else{
-                    console.log('the rider of id: ' + id + ' doesnt exist in db')
-                    return null
-                }
-            })
-          } catch(err){
-              console.log('get rider by id has failed')
-          }
+                .then((res) => {
+                    if (res.status === 200) {
+                        return res.json()
+                    }
+                    else {
+                        console.log('the rider of id: ' + id + ' doesnt exist in db')
+                        return null
+                    }
+                })
+        } catch (err) {
+            console.log('get rider by id has failed')
+        }
     }
 
-    async getAllBySurname(surname){
-        try{
+    async getAllBySurname(surname) {
+        try {
             return await fetch(`http://localhost:8080/rider/getAllBySurname/${surname}`)
-            .then((res) => res.json())
-        }catch(error){
+                .then((res) => res.json())
+        } catch (error) {
             console.log('get all riders by surname failed')
         }
     }
 
     getTheLastRider = async () => {
-        try{
+        try {
             console.log('getTheLastRider db read')
             return await fetch(`http://localhost:8080/rider/getLastRider`)
-            .then((res) => res.json())
-        }catch(err){
+                .then((res) => res.json())
+        } catch (err) {
             console.log('get the last rider failed')
         }
     }
@@ -42,28 +42,41 @@ export default class SpeedwayRider{
     It is a recursive call as its important to make sure all riders has been fetched from the db
     and promise call has been attached to each and every db call
     */
-    async fetchRidersFromDB(match, index){
-        if(index < 0){
+    async fetchRidersFromDB(match, index) {
+        if (index < 0) {
             return match
         }
         return await this.getAllBySurname(match.riders[index].surname)
-        .then((res) => this.concatRiderParserWithRiderDB(match.riders[index], res, match.dateOfGame))
-        .then((res) => 
-            {           
+            .then((res) => this.concatRiderParserWithRiderDB(match.riders[index], res, match.dateOfGame))
+            .then((res) => {
                 match.riders[index] = res;
-                return this.fetchRidersFromDB(match, index - 1)})
+                return this.fetchRidersFromDB(match, index - 1)
+            })
     }
 
-    concatRiderParserWithRiderDB(rider, riderDB, matchDate){
+    concatRiderParserWithRiderDB(rider, riderDB, matchDate) {
         let checkDate = +matchDate.substring(matchDate.lastIndexOf('-') + 1)
-        if(rider.surname.toLowerCase() === 'pawlicki' && checkDate < 1995){
-            rider.name = 'Piotr Sr'
+        if (rider.surname.toLowerCase() === 'pawlicki' && rider.name === 'P') {
+            if (checkDate < 1995) {
+                rider.name = 'Piotr Sr'
+            }
+            else {
+                rider.name = 'Piotr Jr'
+            }
         }
-        else if(rider.surname.toLowerCase() === 'baliński' && rider.name === 'D'){
+        else if (rider.surname.toLowerCase() === 'baliński' && rider.name === 'D') {
             rider.name = 'Dariusz'
         }
+        else if (rider.surname.toLowerCase() === 'liszka') {
+            if (checkDate < 1986) {
+                rider.name = 'Piotr'
+            }
+            else {
+                rider.name = 'Przemysław'
+            }
+        }
         riderDB = riderDB.find((rdr) => rdr.name.substring(0, rider.name.length) === rider.name)
-        if(riderDB){
+        if (riderDB) {
             let numericData = {
                 points: rider.points,
                 bonuses: rider.bonuses,
@@ -72,7 +85,7 @@ export default class SpeedwayRider{
                 fullPerfects: rider.fullPerfects,
                 paidPerfects: rider.paidPerfects
             }
-            for(const key in riderDB){
+            for (const key in riderDB) {
                 rider[key] = riderDB[key]
             }
             rider.points += numericData.points
@@ -86,36 +99,36 @@ export default class SpeedwayRider{
             rider.paidPerfects += numericData.paidPerfects
             rider = this.updateRiderBirthAttribs(rider, matchDate)
             rider.edit = false
-            }
+        }
         return rider
     }
 
     /*
      Card_014
     */
-    updateRiderBirthAttribs(rider, matchDate){
+    updateRiderBirthAttribs(rider, matchDate) {
         let currentDate = new Date().getFullYear()
-        if(rider.ripDate){
-            currentDate = +rider.ripDate.substring(0, 4) 
+        if (rider.ripDate) {
+            currentDate = +rider.ripDate.substring(0, 4)
             rider.ripDate = rider.ripDate.substring(0, 10)
         }
         rider.birthDate = rider.birthDate.substring(0, 10)
-        rider.age = currentDate - +rider.birthDate.substring(0, 4)          
+        rider.age = currentDate - +rider.birthDate.substring(0, 4)
         rider.seasonAge = +matchDate.substring(matchDate.lastIndexOf('-') + 1) - +rider.birthDate.substring(0, 4)
         return rider
     }
 
-    async postNewRider (newRider) {
-        try{
+    async postNewRider(newRider) {
+        try {
             await fetch(`http://localhost:8080/rider/addRider`, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(newRider)        
+                body: JSON.stringify(newRider)
             });
-        }catch(error){
+        } catch (error) {
             console.log('rider post has failed')
         }
     }
@@ -125,28 +138,28 @@ export default class SpeedwayRider{
         all riders already exist in db, same teams
         index - last index of the match.riders list
     */
-    async updateRiders(match, index){
-        if(index < 0)
+    async updateRiders(match, index) {
+        if (index < 0)
             return
         return await this.updateRider(match.riders[index])
-        .then(() => this.updateRiders(match, index - 1))
+            .then(() => this.updateRiders(match, index - 1))
     }
 
     updateRider = async (rdr) => {
         let datka = Date.now();
         rdr.lastUpdated = datka
-        try{
-           await fetch(`http://localhost:8080/rider/updateRider`, {
-               method: 'PUT',
-               headers: {
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(rdr)        
-           });
-           console.log('speedway rider update')
-       }catch(error){
-        console.log('update rider failed')
-       }
+        try {
+            await fetch(`http://localhost:8080/rider/updateRider`, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(rdr)
+            });
+            console.log('speedway rider update')
+        } catch (error) {
+            console.log('update rider failed')
+        }
     }
 }
